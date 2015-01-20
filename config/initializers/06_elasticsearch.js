acd46e1cc2bbc4e737f17e8060d03e3e13873b82
hament90 <vineet@macy.local>
2015-01-20 20:18:37 +0530

    home page UI Changes

--- config/initializers/06_elasticsearch.js
+++ config/initializers/06_elasticsearch.js
@@ -21,7 +21,7 @@ module.exports = function(done) {
 	
 	try{
 		_app_context.esClient = new this.elasticsearch.Client({
-		host: '122.176.210.251:9200',
+		host: '192.168.1.60:9200',
 		log: 'trace'
 		});
 
