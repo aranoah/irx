23131cf42cacdd612db39d5f5778a79e46f13ad5
puneet100 <puneet@aranoah.com>
2015-01-21 21:06:43 +0530

    login popup for mobile

--- config/initializers/06_elasticsearch.js
+++ config/initializers/06_elasticsearch.js
@@ -21,7 +21,7 @@ module.exports = function(done) {
 	
 	try{
 		_app_context.esClient = new this.elasticsearch.Client({
-			host: 'localhost:9200',
+			host: '122.176.206.39:9200',
 			log: 'trace'
 		});
 
