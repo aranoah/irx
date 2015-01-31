174ca75ea17997b580eacf96bad6e6cbfffe2f05
hament90 <vineet@macy.local>
2015-01-31 16:20:53 +0530

    about-us & terms condition

--- config/initializers/06_elasticsearch.js
+++ config/initializers/06_elasticsearch.js
@@ -20,7 +20,7 @@ module.exports = function(done) {
 	
 	try{
 		_app_context.esClient = new this.elasticsearch.Client({
-			host: '122.176.210.254:9200',
+			host: '192.168.1.60:9200',
 			log: 'trace'
 		});
 
