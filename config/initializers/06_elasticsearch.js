8ad47218965566b74d8deaa15ed2862f026ff8f1
puneet100 <puneet@aranoah.com>
2015-01-31 11:38:43 +0530

    logout and settings added

--- config/initializers/06_elasticsearch.js
+++ config/initializers/06_elasticsearch.js
@@ -20,7 +20,7 @@ module.exports = function(done) {
 	
 	try{
 		_app_context.esClient = new this.elasticsearch.Client({
-			host: '122.176.210.254:9200',
+			host: '122.176.230.142:9200',
 			log: 'trace'
 		});
 
