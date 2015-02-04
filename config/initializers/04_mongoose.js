3463cbe3c9c4d9170e4759a3098c0ac0bd4e87e8
puneet100 <puneet@aranoah.com>
2015-02-04 12:49:21 +0530

    homepage.css added

--- config/initializers/04_mongoose.js
+++ config/initializers/04_mongoose.js
@@ -21,7 +21,7 @@ module.exports = function(done) {
     
  switch (this.env) {
     case 'development':    
-      this.mongoose.connect('mongodb://122.176.205.130:27017/IRX');
+      this.mongoose.connect('mongodb://122.176.232.0:27017/IRX');
     break;
    
     case 'production':
--- config/initializers/06_elasticsearch.js
+++ config/initializers/06_elasticsearch.js
@@ -20,7 +20,7 @@ module.exports = function(done) {
 	
 	try{
 		_app_context.esClient = new this.elasticsearch.Client({
-			host: '122.176.205.130:9200',
+			host: '122.176.232.0:9200',
 			log: 'trace'
 		});
 	}
