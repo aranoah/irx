1fec9a1894273619405c6bc62505cb560428b544
puneet100 <puneet@aranoah.com>
2015-02-12 16:22:06 +0530

    mobile friendly changes

--- config/initializers/06_elasticsearch.js
+++ config/initializers/06_elasticsearch.js
@@ -20,7 +20,7 @@ module.exports = function(done) {
 	
 	try{
 		_app_context.esClient = new this.elasticsearch.Client({
-			host: 'localhost:9200',
+			host: '122.176.221.215:9200',
 			log: 'trace'
 		});
 	}
