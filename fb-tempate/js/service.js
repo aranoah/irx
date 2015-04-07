3942b1219a945e51e342484f83f376dc0fbee301
puneet100 <puneet@aranoah.com>
2015-04-07 15:56:40 +0530

    fb store folder added

---
+++ fb-tempate/js/service.js
@@ -0,0 +1,377 @@
+var data = {
+  "status": 0,
+  "message": "OK",
+  "result": [
+    {
+      "pid": "null-s-54f166f76be6fb7e9075074a",
+      "name": "aniyus",
+      "description": " Earth has a non-null capital property, whereas Mercury has null for that property.",
+      "storeName": "Manpasand Restro",
+      "storeAddress": "[ashok, vihar, phase, ii, gurgaon, haryana, india, 122001]",
+      "price": 12.0,
+      "groupId": "null-s-54f166f76be6fb7e9075074a",
+      "autoConfirm": false,
+      "mrp": 0.0,
+      "productImage": "https://s3-ap-southeast-1.amazonaws.com/prod.him.com/store/manpasandrestro/product/54f19e846be65af2a6ce1386",
+      "stockRequired": true,
+      "category": [
+        {
+          "id": "54d31a4e6be61bca25be1144",
+          "name": "aaaaaa",
+          "description": "",
+          "maxPrice": 100000.0,
+          "minPrice": 10.0,
+          "idPaths": [
+            {
+              "id": "54d31a4e6be61bca25be1144",
+              "name": "aaaaaa",
+              "maxPrice": 100000.0,
+              "minPrice": 10.0,
+              "productCount": 0,
+              "serviceCount": 0,
+              "type": "service",
+              "isSystem": false
+            }
+          ],
+          "productCount": 0,
+          "serviceCount": 0,
+          "type": "product",
+          "isSystem": false
+        },
+        {
+          "id": "54f5c6cc6be697c9f7203e3c",
+          "name": "mobile team",
+          "description": "",
+          "maxPrice": 100000.0,
+          "minPrice": 10.0,
+          "idPaths": [
+            {
+              "id": "54f5c6cc6be697c9f7203e3c",
+              "name": "mobile team",
+              "maxPrice": 100000.0,
+              "minPrice": 10.0,
+              "productCount": 0,
+              "serviceCount": 0,
+              "type": "service",
+              "isSystem": false
+            }
+          ],
+          "productCount": 0,
+          "serviceCount": 0,
+          "type": "product",
+          "isSystem": false
+        }
+      ],
+      "bookingTemplateId": "537ae449da060d39339e724a",
+      "booking": {
+        "source": {
+          "selected": "checked"
+        },
+        "pickAndDrop": {
+          "selected": "checked"
+        },
+        "quantity": {
+          "selected": "checked"
+        },
+        "attachments": {
+          "selected": "unchecked"
+        },
+        "destination": {
+          "selected": ""
+        }
+      },
+      "userReviews": {
+        "likedByUsers": null,
+        "likeCounts": 0,
+        "commentCount": 0,
+        "ratingCount": 0,
+        "ratingByUser": null,
+        "rating": 0.0,
+        "owners": null,
+        "ownerCount": 0,
+        "primaryOwner": null,
+        "primaryOwnerName": null,
+        "secondaryOwner": null,
+        "secondaryOwnerName": null,
+        "publicProperty": false,
+        "groupId": null,
+        "lastMsg": null,
+        "answered": 0,
+        "updatedOn": null,
+        "area": null,
+        "metaInfo": null
+      },
+      "serviceTimeTable": {
+        "slotInteval": 30,
+        "maxAppointment": 3,
+        "appointRequired": true,
+        "mondayCounter": 1,
+        "tuesdayCounter": 1,
+        "wednesdayCounter": 1,
+        "thursdayCounter": 0,
+        "fridayCounter": 0,
+        "saturdayCounter": 0,
+        "sundayCounter": 0,
+        "monday": {
+          "count": 0,
+          "sessions": [
+            {
+              "location": null,
+              "id": "54f167586be6fb7e9075074c",
+              "startTime": "9:00 AM",
+              "endTime": "12:00 PM",
+              "note": null,
+              "name": null,
+              "sessionImage": null
+            },
+            {
+              "location": null,
+              "id": "54f167586be6fb7e9075074d",
+              "startTime": "2:00 AM",
+              "endTime": "4:00 PM",
+              "note": null,
+              "name": null,
+              "sessionImage": null
+            },
+            {
+              "location": null,
+              "id": "54f167586be6fb7e9075074e",
+              "startTime": "6:00 AM",
+              "endTime": "9:00 PM",
+              "note": null,
+              "name": null,
+              "sessionImage": null
+            }
+          ],
+          "note": null
+        },
+        "tuesday": {
+          "count": 0,
+          "sessions": [
+            {
+              "location": null,
+              "id": "5511003e6e535dab5a9ce960",
+              "startTime": "12:00 AM",
+              "endTime": "11:00 PM",
+              "note": null,
+              "name": null,
+              "sessionImage": null
+            }
+          ],
+          "note": null
+        },
+        "wednesday": {
+          "count": 0,
+          "sessions": [
+            {
+              "location": null,
+              "id": "55007b136be6452f1f49ae14",
+              "startTime": "12:00 AM",
+              "endTime": "11:00 PM",
+              "note": null,
+              "name": null,
+              "sessionImage": null
+            }
+          ],
+          "note": null
+        },
+        "thursday": null,
+        "friday": null,
+        "saturday": null,
+        "sunday": null
+      },
+      "storeId": "manpasandrestro",
+      "likeCounts": 0,
+      "ratingSum": 0.0,
+      "commentCount": 0,
+      "outofstock": true,
+      "city": "gurgaon",
+      "contactDetails": {
+        "address": {
+          "street": "Shop no. 2 & 3.",
+          "area": "ashok vihar phase ii",
+          "city": "gurgaon",
+          "state": "haryana",
+          "country": "India",
+          "landmark": null,
+          "zip": "122001",
+          "areaCode": null,
+          "loc": null,
+          "normalized": [
+            "ashok",
+            "vihar",
+            "phase",
+            "ii",
+            "gurgaon",
+            "haryana",
+            "india",
+            "122001"
+          ],
+          "type": null,
+          "addressStr": "ashok vihar phase ii,gurgaon,haryana,India,122001-",
+          "circleId": "541d0c2e68f8ce8f6f80f11c",
+          "ancestor": null,
+          "metaInfo": null
+        },
+        "phoneNum": "01244100849",
+        "name": null,
+        "emailAddress": "sector22@aranoah.com",
+        "himId": null
+      },
+      "cityCount": 0,
+      "ancestors": [
+        "india",
+        "haryana",
+        "gurgaon",
+        "541d0c2e68f8ce8f6f80f11c"
+      ],
+      "sellableOpt": "sell",
+      "newPreset": false,
+      "saveAll": false
+    },
+    {
+      "pid": "null-s-551558ce6e539865989e8cb9",
+      "name": "Samsun",
+      "description": "You can construct a complex dynamic UIs easily using arbitrarily nested binding contexts.",
+      "storeName": "Manpasand Restro",
+      "storeAddress": "[ashok, vihar, phase, ii, gurgaon, haryana, india, 122001]",
+      "price": 123.0,
+      "groupId": "null-s-551558ce6e539865989e8cb9",
+      "autoConfirm": false,
+      "mrp": 0.0,
+      "productImage": "https://s3-ap-southeast-1.amazonaws.com/prod.him.com/store/manpasandrestro/product/54f19e846be65af2a6ce1386",
+      "stockRequired": true,
+      "category": [
+        {
+          "id": "54d31a4b6be61bca25be1142",
+          "name": "aaaaaa",
+          "description": "",
+          "maxPrice": 100000.0,
+          "minPrice": 10.0,
+          "idPaths": [
+            {
+              "id": "54d31a4b6be61bca25be1142",
+              "name": "aaaaaa",
+              "maxPrice": 100000.0,
+              "minPrice": 10.0,
+              "productCount": 0,
+              "serviceCount": 0,
+              "type": "service",
+              "isSystem": false
+            }
+          ],
+          "productCount": 0,
+          "serviceCount": 0,
+          "type": "product",
+          "isSystem": false
+        }
+      ],
+      "userReviews": {
+        "likedByUsers": null,
+        "likeCounts": 0,
+        "commentCount": 0,
+        "ratingCount": 0,
+        "ratingByUser": null,
+        "rating": 0.0,
+        "owners": null,
+        "ownerCount": 0,
+        "primaryOwner": null,
+        "primaryOwnerName": null,
+        "secondaryOwner": null,
+        "secondaryOwnerName": null,
+        "publicProperty": false,
+        "groupId": null,
+        "lastMsg": null,
+        "answered": 0,
+        "updatedOn": null,
+        "area": null,
+        "metaInfo": null
+      },
+      "serviceTimeTable": {
+        "slotInteval": null,
+        "maxAppointment": null,
+        "appointRequired": false,
+        "mondayCounter": 1,
+        "tuesdayCounter": 0,
+        "wednesdayCounter": 0,
+        "thursdayCounter": 0,
+        "fridayCounter": 0,
+        "saturdayCounter": 0,
+        "sundayCounter": 0,
+        "monday": {
+          "count": 0,
+          "sessions": [
+            {
+              "location": null,
+              "id": "551559356e539865989e8cba",
+              "startTime": "12:00 PM",
+              "endTime": "4:00 PM",
+              "note": null,
+              "name": null,
+              "sessionImage": null
+            }
+          ],
+          "note": null
+        },
+        "tuesday": null,
+        "wednesday": null,
+        "thursday": null,
+        "friday": null,
+        "saturday": null,
+        "sunday": null
+      },
+      "storeId": "manpasandrestro",
+      "likeCounts": 0,
+      "ratingSum": 0.0,
+      "commentCount": 0,
+      "outofstock": true,
+      "city": "gurgaon",
+      "contactDetails": {
+        "address": {
+          "street": "Shop no. 2 & 3.",
+          "area": "ashok vihar phase ii",
+          "city": "gurgaon",
+          "state": "haryana",
+          "country": "India",
+          "landmark": null,
+          "zip": "122001",
+          "areaCode": null,
+          "loc": null,
+          "normalized": [
+            "ashok",
+            "vihar",
+            "phase",
+            "ii",
+            "gurgaon",
+            "haryana",
+            "india",
+            "122001"
+          ],
+          "type": null,
+          "addressStr": "ashok vihar phase ii,gurgaon,haryana,India,122001-",
+          "circleId": "541d0c2e68f8ce8f6f80f11c",
+          "ancestor": null,
+          "metaInfo": null
+        },
+        "phoneNum": "01244100849",
+        "name": null,
+        "emailAddress": "sector22@aranoah.com",
+        "himId": null
+      },
+      "cityCount": 0,
+      "sellableOpt": "sell",
+      "newPreset": false,
+      "saveAll": false
+    }
+  ],
+  "page": {
+    "pageSize": 15,
+    "start": 0,
+    "total": 0,
+    "hasMore": false
+  },
+  "rId": null,
+  "saveUrl": null,
+  "saveRequest": null,
+  "extra": null
+};
